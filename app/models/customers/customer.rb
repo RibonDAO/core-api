55302961574d412a64c70da70bea11489fc0163f
Henrique Barbosa <henrikleod@gmail.com>
2022-07-11 15:00:15 -0300

    refactor: removing old customer models

--- app/models/customers/customer.rb
+++
@@ -1,9 +0,0 @@
-class Customer < ApplicationRecord
-  include UuidHelper
-
-  belongs_to :user
-  has_many :customer_payments, dependent: :destroy
-
-  validates :email, presence: true
-  validates :name, presence: true
-end
--- app/models/customers/customer_payment_blockchain.rb
+++
@@ -1,9 +0,0 @@
-class CustomerPaymentBlockchain < ApplicationRecord
-  belongs_to :customer_payment
-
-  enum treasure_entry_status: {
-    processing: 0,
-    success: 1,
-    failed: 2
-  }
-end
