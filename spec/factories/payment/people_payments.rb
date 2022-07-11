480df7ae0eb2fb89927195c2887190fb78b67646
Henrique Barbosa <henrikleod@gmail.com>
2022-07-11 15:22:11 -0300

    refactor: people payments factory

--- spec/factories/payment/people_payments.rb
+++ spec/factories/payment/people_payments.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-  factory :customer_payment do
+  factory :people_payment do
     paid_date { '2021-09-20 12:20:41' }
     payment_method { :credit_card }
     status { :paid }
-    association :customer, factory: :customer
+    association :people, factory: :people
     offer { build(:offer) }
   end
 end
