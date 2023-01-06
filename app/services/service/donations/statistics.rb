7f6c0d5c8efab99e8720d7bee48ee2b41225feff
Henrique Barbosa <henrikleod@gmail.com>
2023-01-06 10:37:46 -0300

    refactor: fix chart split behavior

--- app/services/service/donations/statistics.rb
+++ app/services/service/donations/statistics.rb
@@ -18,23 +18,23 @@ module Service
       end
 
       def donations_splitted_into_intervals
-        slice_size = (donations.count / GROUP_INTERVALS).zero? ? 1 : donations.count / GROUP_INTERVALS
+        (start_date.to_i..end_date.to_i).step(days_spacing).map do |date|
+          range_start_date, range_end_date = chart_date_sub_range(date)
 
-        donations.each_slice(slice_size).map do |donations_slice|
           {
-            initial_date: donations_slice.first.created_at.strftime('%d/%m/%Y'),
-            count: donations_slice.count
+            initial_date: range_start_date.strftime('%d/%m/%Y'),
+            count: donations.created_between(range_start_date, range_end_date).count
           }
         end
       end
 
       def donors_splitted_into_intervals
-        slice_size = (donations.count / GROUP_INTERVALS).zero? ? 1 : donations.count / GROUP_INTERVALS
+        (start_date.to_i..end_date.to_i).step(days_spacing).map do |date|
+          range_start_date, range_end_date = chart_date_sub_range(date)
 
-        donations.each_slice(slice_size).map do |donations_slice|
           {
-            initial_date: donations_slice.first.created_at.strftime('%d/%m/%Y'),
-            count: donations_slice.map(&:user_id).uniq.count
+            initial_date: range_start_date.strftime('%d/%m/%Y'),
+            count: donations.created_between(range_start_date, range_end_date).distinct.count(:user_id)
           }
         end
       end
@@ -56,6 +56,21 @@ module Service
 
       private
 
+      def days_spacing
+        @spacing ||= (end_date - start_date) / GROUP_INTERVALS
+        @spacing < 1.day ? 1.day : @spacing
+      end
+
+      def chart_date_sub_range(initial_timestamp)
+        range_start_date = Time.zone.at(initial_timestamp)
+        range_end_date   = Time.zone.at(initial_timestamp + days_spacing)
+
+        [
+          range_start_date,
+          range_end_date > Time.zone.now ? Time.zone.now : range_end_date
+        ]
+      end
+
       def format_impacts(non_profit)
         { non_profit:, impact: impact_sum_by_non_profit(non_profit) }
       end
@@ -76,6 +91,14 @@ module Service
 
       def total_usd_cents_donated_for(non_profit)
         donations.where(non_profit:).sum(&:value)
+      end
+
+      def start_date
+        @start_date ||= donations.order(:created_at).first.created_at
+      end
+
+      def end_date
+        @end_date ||= donations.order(:created_at).last.created_at
       end
 
       def non_profits
