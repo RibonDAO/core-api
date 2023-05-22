18c3f1928210edcc1550d4c2cab05bfde7fba595
mendesmcmg <claraplanejamento@gmail.com>
2023-05-22 09:05:41 -0300

    feat: adds ssl enforcement

--- app/controllers/managers/managers_controller.rb
+++ app/controllers/managers/managers_controller.rb
@@ -1,9 +1,14 @@
 module Managers
   class ManagersController < ActionController::API
+    force_ssl if: :ssl_configured?
     # TODO: implement authentication
     # commented for now, but keeping it here so we don't forget
     # include ApiKeyAuthenticatable
     # prepend_before_action :authenticate_with_api_key!
+
+    def ssl_configured?
+      Rails.env.production?
+    end
 
     rescue_from ActiveRecord::RecordNotFound do |_e|
       render json: { message: 'Not found.' }, status: :not_found
