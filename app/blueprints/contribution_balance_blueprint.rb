d0da10bee112bd21ce38dbd7a2e7d89bd012d0ed
Nicholas Marques <devnicknish@gmail.com>
2023-05-31 17:08:30 -0300

    feat: add contribution_increased_amount_cents to ContributionBalance blueprint

--- app/blueprints/contribution_balance_blueprint.rb
+++ app/blueprints/contribution_balance_blueprint.rb
@@ -1,5 +1,6 @@
 class ContributionBalanceBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :created_at, :updated_at, :increased_amount_cents, :fees_balance_cents, :tickets_balance_cents
+  fields :created_at, :updated_at, :contribution_increased_amount_cents,
+         :fees_balance_cents, :tickets_balance_cents
 end
