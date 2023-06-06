def28c005cbf00c1c9f2adbde79966c7ea28c9c9
Nicholas Marques <devnicknish@gmail.com>
2023-06-06 10:47:39 -0300

    test: add authenticator spec

--- app/lib/jwt/errors.rb
+++ app/lib/jwt/errors.rb
@@ -3,5 +3,6 @@ module Jwt
     class ExpiredSignature < StandardError; end
     class Unauthorized < StandardError; end
     class MissingToken < StandardError; end
+    class InvalidToken < StandardError; end
   end
 end
