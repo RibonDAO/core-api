4864617fd1bf973ef5616b01a04640f84665543d
Nicholas Marques <devnicknish@gmail.com>
2023-06-07 12:18:34 -0300

    fix: lint

--- spec/blueprints/contribution_stats_blueprint_spec.rb
+++ spec/blueprints/contribution_stats_blueprint_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe ContributionStatsBlueprint, type: :blueprint do
   let(:contribution_blueprint) { described_class.render_as_json(contribution_stats) }
 
   it 'has the correct fields' do
-    expect(contribution_blueprint.keys).to match_array(%w[
-                                                         initial_amount used_amount remaining_amount total_tickets
-                                                         avg_donations_per_person boost_amount
-                                                         total_increase_percentage total_amount_to_cause ribon_fee
-                                                       ])
+    expect(contribution_blueprint.keys)
+      .to match_array(%w[
+                        initial_amount used_amount remaining_amount total_tickets
+                        avg_donations_per_person boost_amount
+                        total_increase_percentage total_amount_to_cause ribon_fee
+                      ])
   end
 end
