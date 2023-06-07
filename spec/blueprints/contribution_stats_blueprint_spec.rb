20792340b1b7e699071af682bb43a0f7b082e503
Nicholas Marques <devnicknish@gmail.com>
2023-06-07 12:18:05 -0300

    refactor: update contribution_stats_blueprint test expects

--- spec/blueprints/contribution_stats_blueprint_spec.rb
+++ spec/blueprints/contribution_stats_blueprint_spec.rb
@@ -3,17 +3,13 @@ require 'rails_helper'
 RSpec.describe ContributionStatsBlueprint, type: :blueprint do
   let(:contribution) { create(:contribution) }
   let(:contribution_stats) { Service::Contributions::StatisticsService.new(contribution:).formatted_statistics }
-  let(:contribution_blueprint) { described_class.render(contribution_stats) }
+  let(:contribution_blueprint) { described_class.render_as_json(contribution_stats) }
 
   it 'has the correct fields' do
-    expect(contribution_blueprint).to include('initial_amount')
-    expect(contribution_blueprint).to include('used_amount')
-    expect(contribution_blueprint).to include('remaining_amount')
-    expect(contribution_blueprint).to include('total_tickets')
-    expect(contribution_blueprint).to include('avg_donations_per_person')
-    expect(contribution_blueprint).to include('boost_amount')
-    expect(contribution_blueprint).to include('total_increase_percentage')
-    expect(contribution_blueprint).to include('total_amount_to_cause')
-    expect(contribution_blueprint).to include('ribon_fee')
+    expect(contribution_blueprint.keys).to match_array(%w[
+                                                         initial_amount used_amount remaining_amount total_tickets
+                                                         avg_donations_per_person boost_amount
+                                                         total_increase_percentage total_amount_to_cause ribon_fee
+                                                       ])
   end
 end
