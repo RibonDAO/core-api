80d33e6c8c9974240286806738d7bc604ba26c63
Nicholas Marques <devnicknish@gmail.com>
2023-06-12 09:16:49 -0300

    fix: add initialize to renew_refresh_token

--- app/commands/auth/renew_refresh_token.rb
+++ app/commands/auth/renew_refresh_token.rb
@@ -2,7 +2,14 @@ module Auth
   class RenewRefreshToken < ApplicationCommand
     prepend SimpleCommand
 
-    def call(access_token:, refresh_token:)
+    attr_reader :access_token, :refresh_token
+
+    def initialize(access_token:, refresh_token:)
+      @access_token = access_token
+      @refresh_token = refresh_token
+    end
+
+    def call
       with_exception_handle do
         decoded_token = Jwt::Decoder.decode(token: access_token, custom_options: { verify_expiration: false })
         authenticatable = UserManager.find decoded_token[:authenticatable_id]
