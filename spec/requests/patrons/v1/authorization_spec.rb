375df6da71248ea30ec1f68f2e85262d7d745326
Nicholas Marques <devnicknish@gmail.com>
2023-06-20 15:11:21 -0300

    test: add authorize_from_email_link route specs

--- spec/requests/patrons/v1/authorization_spec.rb
+++ spec/requests/patrons/v1/authorization_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Patrons::V1::Authorization', type: :request do
-  describe 'GET /send_authentication_email' do
+  describe 'POST /send_authentication_email' do
     subject(:request) { post '/patrons/v1/auth/send_authentication_email', params: }
 
     let(:patron) { create(:big_donor, email: 'patron@ribon.io') }
@@ -30,6 +30,51 @@ RSpec.describe 'Patrons::V1::Authorization', type: :request do
 
       it 'returns status unprocessable_entity' do
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'POST /authorize_from_email_link' do
+    subject(:request) { post '/patrons/v1/auth/authorize_from_email_link', params: }
+
+    let(:patron) { create(:big_donor) }
+    let(:params) { { auth_token: 'auth_token', id: patron.id } }
+    let(:command) do
+      command_double(klass: Auth::AuthorizeAuthToken,
+                     success: true, result:)
+    end
+    let(:result) do
+      { access_token: 'access_token',
+        refresh_token: OpenStruct.new({ token: 'refresh_token' }) }
+    end
+
+    before do
+      allow(Auth::AuthorizeAuthToken).to receive(:call).and_return(command)
+      request
+    end
+
+    it 'calls the send authorize command with right params' do
+      expect(Auth::AuthorizeAuthToken).to have_received(:call).with(
+        authenticatable: patron, auth_token: 'auth_token'
+      )
+    end
+
+    context 'when the send authentication link command succeeds' do
+      it 'returns status ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns the access token and refresh token on headers' do
+        expect(response.headers['access-token']).to eq('access_token')
+        expect(response.headers['refresh-token']).to eq('refresh_token')
+      end
+    end
+
+    context 'when the send authentication link command fails' do
+      let(:command) { command_double(klass: Auth::AuthorizeAuthToken, success: false) }
+
+      it 'returns status unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
