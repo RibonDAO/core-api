55fd0d96fe5d2639a2ce8cb96aca452c25c4ba28
Thais Correa <thais@ribon.io>
2023-06-20 16:27:13 -0300

    feat: boost new contributors

--- app/blueprints/contribution_stats_blueprint.rb
+++ app/blueprints/contribution_stats_blueprint.rb
@@ -1,5 +1,6 @@
 class ContributionStatsBlueprint < Blueprinter::Base
   fields :initial_amount, :used_amount, :usage_percentage, :remaining_amount,
          :total_tickets, :avg_donations_per_person, :boost_amount,
-         :total_increase_percentage, :total_amount_to_cause, :ribon_fee
+         :total_increase_percentage, :total_amount_to_cause, :ribon_fee,
+         :boost_new_contributors, :boost_new_patrons
 end
