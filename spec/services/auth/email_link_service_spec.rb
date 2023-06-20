66dc55e37dc2ebdf389b2620d6d1c3a673754c64
Nicholas Marques <devnicknish@gmail.com>
2023-06-20 10:48:22 -0300

    test: add email_link_service #find_or_create_auth_link tests

---
+++ spec/services/auth/email_link_service_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Auth::EmailLinkService, type: :service do
+  subject(:service) { described_class.new(authenticatable:) }
+
+  let(:authenticatable) { create(:big_donor) }
+
+  describe '#find_or_create_auth_link' do
+    context 'when there is already an auth token for that authenticatable' do
+      let(:auth_token) { 'auth_token' }
+
+      before do
+        allow(SecureRandom).to receive(:uuid).and_return(auth_token)
+        service.send(:generate_new_auth_token)
+      end
+
+      it 'returns the link with that auth token' do
+        expect(service.find_or_create_auth_link)
+          .to eq(
+            "#{RibonCoreApi.config[:patrons][:app][:url]}/auth?authToken=#{auth_token}&id=#{authenticatable.id}"
+          )
+      end
+    end
+
+    context 'when there is no auth token' do
+      let(:new_auth_token) { 'new_auth_token' }
+
+      before do
+        allow(SecureRandom).to receive(:uuid).and_return(new_auth_token)
+      end
+
+      it 'returns the link with a new auth token' do
+        expect(service.find_or_create_auth_link).to eq(
+          "#{RibonCoreApi.config[:patrons][:app][:url]}/auth?authToken=#{new_auth_token}&id=#{authenticatable.id}"
+        )
+      end
+    end
+  end
+end
