b098a238c0269bd012585bd9ecf40084ee02db0d
Juju <juju@ribon.io>
2023-06-26 11:42:58 -0300

    Fix tests

--- spec/services/service/contributions/statistics_service_spec.rb
+++ spec/services/service/contributions/statistics_service_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Service::Contributions::StatisticsService, type: :service do
         .to match_array(%i[initial_amount used_amount usage_percentage
                            remaining_amount total_tickets avg_donations_per_person
                            boost_amount total_increase_percentage total_amount_to_cause ribon_fee
-                           boost_new_contributors boost_new_patrons])
+                           boost_new_contributors boost_new_patrons total_donors total_contributors])
     end
   end
 
