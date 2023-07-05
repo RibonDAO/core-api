7be88986ba4d952efe6b943132de64becd739de5
mendesmcmg <claraplanejamento@gmail.com>
2023-07-05 15:21:06 -0300

    test: adds emailLog test

--- spec/models/email_log_spec.rb
+++ spec/models/email_log_spec.rb
@@ -14,5 +14,52 @@
 require 'rails_helper'
 
 RSpec.describe EmailLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:receiver) }
+  end
+
+  describe 'enums' do
+    it { is_expected.to define_enum_for(:email_type).with_values(default: 0, patron_contribution: 1) }
+    it { is_expected.to define_enum_for(:status).with_values(enqueued: 0, sent: 1) }
+  end
+
+  describe '.email_already_sent?' do
+    let(:receiver) { create(:big_donor) }
+    let(:sendgrid_template_name) { 'template_name' }
+
+    context 'when email log exists for the receiver and template name' do
+      before do
+        create(:email_log, receiver:, sendgrid_template_name:)
+      end
+
+      it 'returns true' do
+        expect(described_class.email_already_sent?(receiver:, sendgrid_template_name:)).to be true
+      end
+    end
+
+    context 'when email log does not exist for the receiver and template name' do
+      it 'returns false' do
+        expect(described_class.email_already_sent?(receiver:, sendgrid_template_name:)).to be false
+      end
+    end
+  end
+
+  describe '.log' do
+    let(:sendgrid_template_name) { 'template_name' }
+    let(:email_type) { :patron_contribution }
+    let(:receiver) { create(:big_donor) }
+
+    it 'creates a new email log' do
+      expect do
+        described_class.log(sendgrid_template_name:, email_type:, receiver:)
+      end.to change(described_class, :count).by(1)
+    end
+
+    it 'sets the attributes correctly' do
+      email_log = described_class.log(sendgrid_template_name:, email_type:, receiver:)
+      expect(email_log.sendgrid_template_name).to eq(sendgrid_template_name)
+      expect(email_log.email_type).to eq(email_type.to_s)
+      expect(email_log.receiver).to eq(receiver)
+    end
+  end
 end
