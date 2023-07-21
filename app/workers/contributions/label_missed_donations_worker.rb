543a0a777330d66d585af29cdae879e238bec209
Karina Almeida <s.karinaalmeida@gmail.com>
2023-07-21 12:17:38 -0300

    refactor: removed unnecessary Service:: from rest of files

--- app/workers/contributions/label_missed_donations_worker.rb
+++ app/workers/contributions/label_missed_donations_worker.rb
@@ -5,7 +5,7 @@ module Contributions
 
     def perform(*_args)
       Donation.without_label.find_each do |donation|
-        Service::Contributions::TicketLabelingService.new(donation:).label_donation
+        Contributions::TicketLabelingService.new(donation:).label_donation
       end
     rescue StandardError => e
       Reporter.log(error: e, extra: { message: e.message })
