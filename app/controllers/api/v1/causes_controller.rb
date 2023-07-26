18ca7f127a0cbb79c1f793f1dc9a3280e8e3a5bb
letbueno <leticia.bueno.martins@gmail.com>
2023-07-26 13:50:44 -0300

    feat: add shuffle to randomize the array of causes

--- app/controllers/api/v1/causes_controller.rb
+++ app/controllers/api/v1/causes_controller.rb
@@ -2,15 +2,17 @@ module Api
   module V1
     class CausesController < ApplicationController
       def index
-        @causes = Cause.all
+        @causes = Cause.all.shuffle
 
         render json: CauseBlueprint.render(@causes, view: :data_and_images)
       end
 
       def free_donation_causes
         @causes = CauseQueries.new.active_with_pool_balance
+        @random_causes = @causes.shuffle
 
-        render json: CauseBlueprint.render(@causes, view: :data_and_images)
+
+        render json: CauseBlueprint.render(@random_causes, view: :data_and_images)
       end
 
       def create
