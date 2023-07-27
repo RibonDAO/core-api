75628562952a1fa00c97132040557072a24e53e9
Karina Almeida <s.karinaalmeida@gmail.com>
2023-07-27 17:46:33 -0300

    feat: add omniauth method and provider on admin model

--- app/models/users/admin.rb
+++ app/models/users/admin.rb
@@ -5,15 +5,27 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  provider               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  uid                    :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %w[google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |admin|
+      admin.email = auth.info.email
+      admin.password = Devise.friendly_token[0, 20]
+      admin.save
+    end
+  end
 end
