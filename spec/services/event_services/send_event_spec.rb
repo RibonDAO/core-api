ab739ac75820ba9286b1d9aad31ae73eb4f6d978
Juju <juju@ribon.io>
2023-08-03 14:59:46 -0300

    Fix linter

--- spec/services/event_services/send_event_spec.rb
+++ spec/services/event_services/send_event_spec.rb
@@ -5,19 +5,21 @@ RSpec.describe EventServices::SendEvent, type: :service do
 
   describe '#send_event' do
     let(:user) { create(:user) }
-    
-    let(:event) { OpenStruct.new({
-      name: "purchase", 
-      data: { 
-        price: 1000,
-        quantity: 1 
-      }
-    })}
+
+    let(:event) do
+      OpenStruct.new({
+                       name: 'purchase',
+                       data: {
+                         price: 1000,
+                         quantity: 1
+                       }
+                     })
+    end
 
     include_context('when mocking a request') { let(:cassette_name) { 'send_event_customer' } }
 
     it 'send event track' do
-      expect(service.call.code).to eq("200")
+      expect(service.call.code).to eq('200')
     end
   end
 end
