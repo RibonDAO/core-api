2dcefb856b3b9b4766cc0a20004a568ee17d265b
Nicholas Marques <devnicknish@gmail.com>
2023-08-03 10:24:18 -0300

    chore: add missing annotate to models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_31_183207) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -85,7 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_183207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "link"
-    t.integer "language", default: 0
+    t.string "language"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
@@ -370,6 +369,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_183207) do
     t.integer "donations_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_email"
+    t.integer "user_legacy_id"
+    t.datetime "user_created_at"
     t.bigint "legacy_user_id"
     t.index ["legacy_non_profit_id"], name: "index_legacy_user_impacts_on_legacy_non_profit_id"
     t.index ["legacy_user_id"], name: "index_legacy_user_impacts_on_legacy_user_id"
@@ -417,8 +419,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_183207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "impact_description"
-    t.string "donor_recipient"
     t.string "measurement_unit"
+    t.string "donor_recipient"
     t.index ["non_profit_id"], name: "index_non_profit_impacts_on_non_profit_id"
   end
 
@@ -586,6 +588,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_183207) do
     t.index ["receiver_type", "receiver_id"], name: "index_subscriptions_on_receiver"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "actions"
+    t.text "rules"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tokens", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -653,7 +663,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_183207) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "language", default: 0
+    t.integer "language"
     t.integer "legacy_id"
     t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
