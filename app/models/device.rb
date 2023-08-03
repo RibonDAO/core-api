ba6b5438f17bd24cae1c7be1d71d187baf92d36c
Juju <juju@ribon.io>
2023-08-03 14:22:05 -0300

    Remove validation on devices table

--- app/models/device.rb
+++ app/models/device.rb
@@ -12,6 +12,5 @@
 class Device < ApplicationRecord
   belongs_to :user
 
-  validates :device_id, :device_token, presence: true
-  validates :device_id, uniqueness: { scope: :user_id }
+  validates :device_token, uniqueness, presence: true
 end
