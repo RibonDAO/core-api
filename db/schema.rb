2308c62498d1cde45a0429de300bc37b58274f41
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-09 17:04:24 -0300

    feat: maintaining pattern using fk true

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_172133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "link"
-    t.string "language"
+    t.integer "language", default: 0
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
@@ -369,9 +369,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_172133) do
     t.integer "donations_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_email"
-    t.integer "user_legacy_id"
-    t.datetime "user_created_at"
     t.bigint "legacy_user_id"
     t.index ["legacy_non_profit_id"], name: "index_legacy_user_impacts_on_legacy_non_profit_id"
     t.index ["legacy_user_id"], name: "index_legacy_user_impacts_on_legacy_user_id"
@@ -419,8 +416,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_172133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "impact_description"
-    t.string "measurement_unit"
     t.string "donor_recipient"
+    t.string "measurement_unit"
     t.index ["non_profit_id"], name: "index_non_profit_impacts_on_non_profit_id"
   end
 
@@ -590,14 +587,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_172133) do
     t.index ["receiver_type", "receiver_id"], name: "index_subscriptions_on_receiver"
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "actions"
-    t.text "rules"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tokens", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -665,7 +654,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_172133) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "language"
+    t.integer "language", default: 0
     t.integer "legacy_id"
     t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -747,6 +736,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_172133) do
   add_foreign_key "pools", "causes"
   add_foreign_key "pools", "tokens"
   add_foreign_key "stories", "non_profits"
+  add_foreign_key "subscriptions", "offers"
   add_foreign_key "user_completed_tasks", "users"
   add_foreign_key "user_donation_stats", "users"
   add_foreign_key "user_tasks_statistics", "users"
