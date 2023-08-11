3f1f22b26882a0bc7ad2739b81ff3af925f1cd0a
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-11 11:03:12 -0300

    feat: add subscription on admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -37,7 +37,7 @@ RailsAdmin.config do |config|
                             Voucher, IntegrationWebhook, Token, Pool, PoolBalance, History, BalanceHistory,
                             LegacyUserImpact, LegacyNonProfit, Article, Author, LegacyContribution,
                             ContributionBalance, PersonBlockchainTransaction, DonationContribution, BigDonor,
-                            LegacyUser, LegacyIntegrationImpact, LegacyIntegration, Device]
+                            LegacyUser, LegacyIntegrationImpact, LegacyIntegration, Device, Subscription]
 
   config.model RibonConfig do
     field :default_ticket_value do
