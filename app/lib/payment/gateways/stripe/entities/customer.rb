0822167fde7e7e93b50be93302e159446de16736
Nicholas Marques <devnicknish@gmail.com>
2023-08-15 16:08:37 -0300

    feat: add default address to stripe

--- app/lib/payment/gateways/stripe/entities/customer.rb
+++ app/lib/payment/gateways/stripe/entities/customer.rb
@@ -3,15 +3,24 @@ module Payment
     module Stripe
       module Entities
         class Customer
-          def self.create(customer:, payment_method:)
+          def self.create(customer:, payment_method:, address: default_address)
             ::Stripe::Customer.create({
                                         email: customer&.email,
                                         name: customer&.name,
                                         payment_method: payment_method&.id,
                                         invoice_settings: {
                                           default_payment_method: payment_method&.id
-                                        }
+                                        },
+                                        address:
                                       })
+          end
+
+          def self.default_address
+            {
+              country: 'BR',
+              city: 'Brasília',
+              state: 'DF'
+            }
           end
         end
       end
