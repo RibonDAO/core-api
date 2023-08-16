3e714739fdb287ea1d29ebbd8a4bef7410ef3155
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-16 14:38:29 -0300

    feat: add cancel subscription controller

---
+++ app/controllers/api/v1/payments/subscriptions_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    module Payments
+      class SubscriptionsController < ApplicationController
+        include ::Givings::Payment
+
+        def unsubscribe
+          command = ::Givings::Payment::CancelSubscription.call(subscription_id: params[:id])
+
+          if command.success?
+            head :ok
+          else
+            render_errors(command.errors)
+          end
+        end
+
+        private
+
+        def subscription_params
+          params.permit(:id)
+        end
+      end
+    end
+  end
+end
