527c6f42a2a5295c6cffa0a71de9706bd16c9b8a
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-16 14:02:59 -0300

    feat: cancel subscription command

---
+++ app/commands/givings/payment/cancel_subscription.rb
@@ -0,0 +1,44 @@
+module Givings
+  module Payment
+    class CancelSubscription < ApplicationCommand
+      prepend SimpleCommand
+
+      attr_reader :subscription_id
+
+      def initialize(args)
+        @subscription = args[:subscription_id]
+      end
+
+      def call
+        if subscription&.external_id?
+          unsubscribe = Service::Givings::Payment::Orchestrator.new(payload: cancel_params).call
+        end
+
+        success_unsubscribe(subscription, unsubscribe)
+      rescue StandardError => e
+        Reporter.log(error: e, extra: { message: e.message }, level: :fatal)
+      end
+
+      private
+
+      def success_unsubscribe(subscription, unsubscribe)
+        return unless unsubscribe[:status] == 'canceled'
+
+        subscription.update(status: :canceled, cancel_date: Time.zone.at(unsubscribe[:canceled_at]))
+      end
+
+      def subscription
+        Subscription.find_by(id: subscription_id)
+      end
+
+      def gateway
+        subscription.offer.gateway
+      end
+
+      def cancel_params
+        OpenStruct.new(external_identifier: subscription.external_id, operation: 'unsubscribe',
+                       gateway:)
+      end
+    end
+  end
+end
