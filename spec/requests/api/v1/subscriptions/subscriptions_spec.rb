a4d534fcaa960bae33b643fd9f6d9623ee67465a
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-24 15:00:07 -0300

    feat: moved controller spec to new namespace

--- spec/requests/api/v1/subscriptions/subscriptions_spec.rb
+++ spec/requests/api/v1/subscriptions/subscriptions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Api::V1::Payments::Subscriptions', type: :request do
+RSpec.describe 'Api::V1::Subscriptions::Subscriptions', type: :request do
   let(:subscription) { create(:subscription) }
   let(:params) { subscription.id }
   let(:create_order_command_double) do
@@ -11,9 +11,9 @@ RSpec.describe 'Api::V1::Payments::Subscriptions', type: :request do
     allow(Givings::Payment::CancelSubscription).to receive(:call).and_return(create_order_command_double)
   end
 
-  describe 'PUT /api/v1/payments/cancel_subscription/:id' do
+  describe 'PUT /api/v1/subscriptions/cancel_subscription/:id' do
     context 'when is successfully cancelled' do
-      subject(:request) { put "/api/v1/payments/cancel_subscription/#{subscription.id}" }
+      subject(:request) { put "/api/v1/subscriptions/cancel_subscription/#{subscription.id}" }
 
       let(:create_order_command_double) do
         command_double(klass: Givings::Payment::CancelSubscription, success: true)
@@ -27,7 +27,7 @@ RSpec.describe 'Api::V1::Payments::Subscriptions', type: :request do
     end
 
     context 'when command returns error' do
-      subject(:request) { put "/api/v1/payments/cancel_subscription/#{subscription.id}" }
+      subject(:request) { put "/api/v1/subscriptions/cancel_subscription/#{subscription.id}" }
 
       let(:create_order_command_double) do
         command_double(klass: Givings::Payment::CancelSubscription, success: false)
