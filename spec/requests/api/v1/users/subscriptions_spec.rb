879af3e672ed53f243cae1aed95af158949d82be
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-24 18:02:03 -0300

    feat: fix subscriptions spec

--- spec/requests/api/v1/users/subscriptions_spec.rb
+++ spec/requests/api/v1/users/subscriptions_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users::Subscriptions', type: :request do
   describe 'GET /api/v1/users/:user_id/subscriptions' do
-    context 'when is successfully get user subscriptions' do
+    context 'when successfully get user subscriptions' do
       subject(:request) { get "/api/v1/users/#{user.id}/subscriptions" }
+
+      include_context('when mocking a request') { let(:cassette_name) { 'conversion_rate_brl_usd' } }
 
       let(:user) { create(:user) }
       let(:customer) { create(:customer, user:) }
@@ -19,7 +21,8 @@ RSpec.describe 'Api::V1::Users::Subscriptions', type: :request do
 
       it 'returns all user subscriptions' do
         expect(response).to have_http_status(:ok)
-        expect_response_collection_to_have_keys(%w[id status cancel_date created_at offer receiver])
+        expect_response_collection_to_have_keys(%w[id status cancel_date created_at offer receiver
+                                                   person_payments])
       end
     end
   end
