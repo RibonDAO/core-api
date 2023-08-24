8e3296e627958356da9082c98ca6733d79010293
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-24 15:23:01 -0300

    feat: fix spec and readd route

--- config/routes.rb
+++ config/routes.rb
@@ -104,6 +104,7 @@ Rails.application.routes.draw do
         get 'labelable_contributions' => 'users/contributions#labelable'
         get 'contributions/:id' => 'users/contributions#show'
         post 'devices' => 'users/devices#create'
+        get 'subscriptions' => 'users/subscriptions#index'
         post 'send_cancel_subscription_email' => 'users/subscriptions#send_cancel_subscription_email'
       end
       resources :integrations, only: [] do
