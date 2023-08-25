e53cb5e287d6863af46123730c39311a2e1e329e
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-25 16:07:30 -0300

    refactor: returned method on create contribution

--- app/commands/contributions/create_contribution.rb
+++ app/commands/contributions/create_contribution.rb
@@ -15,6 +15,7 @@ module Contributions
       ActiveRecord::Base.transaction do
         contribution = Contribution.create!(person_payment: payment, receiver: payment.receiver,
                                             generated_fee_cents: payment.usd_value_cents * CONTRACT_FEE_PERCENTAGE)
+        handle_contribution_fees(contribution)
         contribution.set_contribution_balance
       end
     rescue StandardError => e
