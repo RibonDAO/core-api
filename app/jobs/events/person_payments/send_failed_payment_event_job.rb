ddf2a7827b5316c6dc21b2396ac7692398b982d4
Nathiely Macedo <nathielymm@gmail.com>
2023-08-28 19:03:40 -0300

    feat: adds failed payment event

--- app/jobs/events/person_payments/send_failed_payment_event_job.rb
+++ app/jobs/events/person_payments/send_failed_payment_event_job.rb
@@ -1,6 +1,6 @@
 module Events
   module PersonPayments
-    class SendPersonPaymentEventJob < ApplicationJob
+    class SendFailedPaymentEventJob < ApplicationJob
       queue_as :default
       sidekiq_options retry: 3
 
@@ -16,27 +16,22 @@ module Events
 
       private
 
-      # rubocop:disable Metrics/MethodLength
       def build_event(person_payment)
         OpenStruct.new({
-                         name: 'subscribed',
+                         name: 'failed_payment',
                          data: {
-                           contribution_id: person_payment.contribution_id,
                            integration_id: person_payment.integration_id,
                            receiver_type: person_payment.receiver_type,
                            receiver_id: person_payment.receiver_id,
                            currency: person_payment.currency,
                            platform: person_payment.platform,
                            amount: person_payment.formatted_amount,
-                           paid_date: person_payment.paid_date.strftime('%d/%m/%Y'),
                            status: person_payment.status,
                            offer_id: person_payment.offer_id,
-                           total_number_of_contributions: person_payment.payer.contributions.count,
                            receiver_name: person_payment.receiver.name
                          }
                        })
       end
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
