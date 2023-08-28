3513adf97fcf2054802a74ecc3279da626bafde3
Nicholas Marques <devnicknish@gmail.com>
2023-08-28 11:50:07 -0300

    feat: add delete contribution fees and donation contribution to each iteration

--- app/services/labeling/relabel_service.rb
+++ app/services/labeling/relabel_service.rb
@@ -10,14 +10,28 @@ module Labeling
       ordered_records.each do |record|
         case record.class.to_s
         when 'Donation'
-          Service::Contributions::TicketLabelingService.new(donation: record).label_donation
+          deal_with_donation(record)
         when 'Contribution'
-          Service::Contributions::FeesLabelingService.new(contribution: record).spread_fee_to_payers
+          deal_with_contribution(record)
         end
       end
     end
 
     private
+
+    def deal_with_donation(donation)
+      ActiveRecord::Base.transaction do
+        donation.donation_contribution&.delete
+        Service::Contributions::TicketLabelingService.new(donation:).label_donation
+      end
+    end
+
+    def deal_with_contribution(contribution)
+      ActiveRecord::Base.transaction do
+        contribution.contribution_fees&.delete_all
+        Service::Contributions::FeesLabelingService.new(contribution:).spread_fee_to_payers
+      end
+    end
 
     def ordered_records
       @ordered_records ||= combined_records.sort_by do |record|
