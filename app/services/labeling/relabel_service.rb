3cdfae2f0bb4f148f249cf9e6bdc61efcd0987de
Nicholas Marques <devnicknish@gmail.com>
2023-08-29 09:02:02 -0300

    feat: add delete records to relabel service

--- app/services/labeling/relabel_service.rb
+++ app/services/labeling/relabel_service.rb
@@ -7,6 +7,8 @@ module Labeling
     end
 
     def relabel
+      setup_records
+
       ordered_records.each do |record|
         case record.class.to_s
         when 'Donation'
@@ -17,25 +19,44 @@ module Labeling
       end
     end
 
+    def setup_records
+      ActiveRecord::Base.transaction do
+        ContributionBalance.delete_all
+        Contributions.all.each(&:set_contribution_balance)
+        ContributionFee.delete_all
+        DonationContribution.delete_all
+      end
+    end
+
+    def ordered_records
+      @ordered_records ||= combined_records.sort_by do |record|
+        if record.class.to_s == 'Donation'
+          record.created_at
+        else
+          record.person_payment&.person_blockchain_transaction&.succeeded_at || record.created_at
+        end
+      end
+    end
+
     private
 
     def deal_with_donation(donation)
+      donation_contribution = donation.donation_contribution
+
       ActiveRecord::Base.transaction do
-        donation.donation_contribution&.delete
+        if donation_contribution
+          Service::Contributions::DonationContributionDeleteService.new(donation_contribution:).delete
+        end
         Service::Contributions::TicketLabelingService.new(donation:).label_donation
       end
     end
 
     def deal_with_contribution(contribution)
       ActiveRecord::Base.transaction do
-        contribution.contribution_fees&.delete_all
+        contribution.contribution_fees.each do |contribution_fee|
+          Service::Contributions::ContributionFeeDeleteService.new(contribution_fee:).handle_fee_delete
+        end
         Service::Contributions::FeesLabelingService.new(contribution:).spread_fee_to_payers
-      end
-    end
-
-    def ordered_records
-      @ordered_records ||= combined_records.sort_by do |record|
-        [record.created_at || record.person_payment&.person_blockchain_transaction&.succeeded_at]
       end
     end
 
