1fc00d51c276f49d6349b1ad1ae003356ec05c65
Nicholas Marques <devnicknish@gmail.com>
2023-10-03 13:42:51 -0300

    feat: add UserConfig model

---
+++ app/models/users/user_config.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: user_configs
+#
+#  id                      :bigint           not null, primary key
+#  allowed_email_marketing :boolean
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  user_id                 :bigint           not null
+#
+class UserConfig < ApplicationRecord
+  belongs_to :user
+end
