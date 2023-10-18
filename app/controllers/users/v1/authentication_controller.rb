c314e62bf6a2baf60a4f0c4a9b6de6a7473a2046
mendesmcmg <claraplanejamento@gmail.com>
2023-10-18 10:30:10 -0300

    refactor: removes email unused code

--- app/controllers/users/v1/authentication_controller.rb
+++ app/controllers/users/v1/authentication_controller.rb
@@ -1,31 +1,7 @@
 module Users
   module V1
     class AuthenticationController < Users::AuthorizationController
-      skip_before_action :authenticate, only: %i[send_authentication_email authorize_from_auth_token refresh_token]
-
-      def send_authentication_email
-        authenticatable = User.find_by!(email: params[:email])
-        command = Auth::SendAuthenticationLink.call(authenticatable:)
-
-        if command.success?
-          render json: { message: I18n.t('users.email_sent') }, status: :ok
-        else
-          render_errors(command.errors, :unprocessable_entity)
-        end
-      end
-
-      def authorize_from_auth_token
-        authenticatable = User.find(params[:id])
-        command = Auth::AuthorizeAuthToken.call(auth_token: params[:auth_token], authenticatable:)
-
-        if command.success?
-          create_headers(command.result)
-
-          render json: UserBlueprint.render(authenticatable), status: :ok
-        else
-          render_errors(command.errors, :unauthorized)
-        end
-      end
+      skip_before_action :authenticate, only: %i[refresh_token]
 
       def refresh_token
         access_token = request.headers['Authorization']&.split('Bearer ')&.last
