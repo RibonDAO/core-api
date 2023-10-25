9edb24b9cc323451d34fcfb90dc32bd80353b1a8
Thais Correa <thais@ribon.io>
2023-10-25 13:07:42 -0300

    fix: remove sentry from dapp-dev

--- app/lib/reporter.rb
+++ app/lib/reporter.rb
@@ -2,11 +2,6 @@
 
 class Reporter
   def self.log(error:, extra: {}, level: :error)
-    if Rails.env.production?
-      Sentry.capture_exception(error, level:, extra:)
-    else
-      Rails.logger.info(error)
-      Rails.logger.info(extra)
-    end
+    Sentry.capture_exception(error, level:, extra:) if Rails.env.production?
   end
 end
