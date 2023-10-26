bd489f0a436d4a961e1634cb88ab4c87bd68a8fc
Henrique Barbosa <henrikleod@gmail.com>
2023-10-26 10:59:00 -0300

    chore: validations for impression card

--- app/controllers/api/v1/impression_cards_controller.rb
+++ app/controllers/api/v1/impression_cards_controller.rb
@@ -2,9 +2,13 @@ module Api
   module V1
     class ImpressionCardsController < ApplicationController
       def show
-        @impression_card = ImpressionCard.find(params[:id])
+        @impression_card = ImpressionCard.find_by(id: params[:id], active: true)
 
-        render json: ImpressionCardBlueprint.render(@impression_card)
+        if(@impression_card)
+          render json: ImpressionCardBlueprint.render(@impression_card)
+        else
+          render json: { error: 'Impression card not found' }, status: :not_found
+        end
       end
     end
   end
