daade456420ae80637fcc0e3b72004f60edb7375
Nathiely Macedo <nathielymm@gmail.com>
2024-01-10 08:16:37 -0300

    feat: adds tickets donation

--- spec/requests/users/v1/tickets/tickets_controller_spec.rb
+++ spec/requests/users/v1/tickets/tickets_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Users::V1::Tickets::Collect', type: :request do
+RSpec.describe 'Users::V1::Tickets', type: :request do
   describe 'POST /can_collect_by_integration' do
     include_context 'when making a user request' do
       subject(:request) { post '/users/v1/tickets/can_collect_by_integration', headers:, params: }
@@ -124,6 +124,60 @@ RSpec.describe 'Users::V1::Tickets::Collect', type: :request do
         request
 
         expect(response).to have_http_status :not_found
+      end
+    end
+  end
+
+  describe 'POST /donate' do
+    include_context 'when making a user request' do
+      subject(:request) { post '/users/v1/tickets/donate', headers:, params: }
+    end
+
+    context 'with right params' do
+      let(:integration) { create(:integration) }
+      let(:non_profit) { create(:non_profit, :with_impact) }
+      let(:user) { create(:user) }
+      let(:platform) { 'web' }
+      let(:params) do
+        {
+          integration_id: integration.id,
+          platform:,
+          non_profit_id: non_profit.id,
+          email: user.email,
+          quantity: 1
+        }
+      end
+
+      before do
+        create(:chain)
+        create_list(:ticket, 2, user:, integration:)
+        create(:ribon_config, default_ticket_value: 100)
+        allow(Tickets::Donate).to receive(:call)
+          .and_return(command_double(klass: Tickets::Donate))
+      end
+
+      it 'calls the donate command with right params' do
+        request
+
+        expect(Tickets::Donate).to have_received(:call).with(
+          integration:,
+          user:,
+          platform:,
+          non_profit:,
+          quantity: '1'
+        )
+      end
+
+      it 'returns success' do
+        request
+
+        expect(response).to have_http_status :ok
+      end
+
+      it 'returns the donation' do
+        request
+
+        expect_response_to_have_keys(%w[donation])
       end
     end
   end
