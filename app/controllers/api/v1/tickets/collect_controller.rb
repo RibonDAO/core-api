3aac67e2a07480d621fecdad8677a5550e6616b2
Nathiely Macedo <nathielymm@gmail.com>
2024-01-11 08:58:57 -0300

    feat: adds first donation route

--- app/controllers/api/v1/tickets/collect_controller.rb
+++ app/controllers/api/v1/tickets/collect_controller.rb
@@ -23,6 +23,16 @@ module Api
           end
         end
 
+        def collect_and_donate_by_integration
+          command = ::Tickets::CollectAndDonateByIntegration.call(integration:, user:, platform:, non_profit:)
+          if command.success?
+            ::Tracking::AddUtm.call(utm_params:, trackable: command.result)
+            render json: { donation: command.result }, status: :ok
+          else
+            render_errors(command.errors)
+          end
+        end
+
         private
 
         def integration
@@ -38,7 +48,11 @@ module Api
         end
 
         def ticket_params
-          params.permit(:integration_id, :email, :platform)
+          params.permit(:integration_id, :email, :platform, :non_profit_id)
+        end
+
+        def non_profit
+          @non_profit ||= NonProfit.find ticket_params[:non_profit_id]
         end
 
         def utm_params
