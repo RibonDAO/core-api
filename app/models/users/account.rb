9756246ba6cd03c703647a149600df294cbb3096
Thais Correa <thais@ribon.io>
2024-02-08 11:15:34 -0300

    fix: annotate

--- app/models/users/account.rb
+++ app/models/users/account.rb
@@ -2,16 +2,15 @@
 #
 # Table name: accounts
 #
-#  id                  :bigint           not null, primary key
-#  confirmed_at        :datetime
-#  deleted_at          :datetime
-#  provider            :string
-#  remember_created_at :datetime
-#  tokens              :json
-#  uid                 :string
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
-#  user_id             :bigint           not null
+#  id           :bigint           not null, primary key
+#  confirmed_at :datetime
+#  deleted_at   :datetime
+#  provider     :string
+#  tokens       :json
+#  uid          :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
 #
 class Account < ApplicationRecord
   include AuthenticatableModel
