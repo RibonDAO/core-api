a783e56b0d4c12d33cd92cdd8c412bc1d4b19ab6
Juju <juju@ribon.io>
2024-03-04 11:49:31 -0300

    fix: add categories and source on build donation

--- app/commands/tickets/donate.rb
+++ app/commands/tickets/donate.rb
@@ -20,11 +20,11 @@ module Tickets
 
     private
 
-    def build_donations(integrations)
+    def build_donations(integrations, sources, categories)
       donations_array = []
       quantity.times do |index|
         donations_array << { integration_id: integrations[index], non_profit_id: non_profit.id, user_id: user.id,
-                             platform:, value: ticket_value, category: :daily, source: :integration }
+                             platform:, value: ticket_value, category: categories[index], source: sources[index] }
       end
 
       donations_array
@@ -36,7 +36,7 @@ module Tickets
         integrations = destroy_result[:integrations]
         external_ids = destroy_result[:external_ids]
 
-        @donations = create_donations(build_donations(integrations))
+        @donations = create_donations(build_donations(integrations, sources, categories))
         associate_integration_vouchers(external_ids)
         update_user_donations_info
         label_donations
@@ -81,8 +81,10 @@ module Tickets
       tickets = Ticket.where(user:).order(created_at: :asc).limit(quantity).destroy_all
       integrations = tickets.pluck(:integration_id)
       external_ids = tickets.pluck(:external_id)
+      sources = tickets.pluck(:source)
+      categories = tickets.pluck(:category)
 
-      { integrations:, external_ids: external_ids.compact }
+      { integrations:, external_ids: external_ids.compact, sources:, categories: }
     end
 
     def update_user_donations_info
