20706dfa768622665b91dcabb305b992d2081268
mendesmcmg <claraplanejamento@gmail.com>
2024-03-07 18:10:40 -0300

    refactor: uses keyword args instead of positional

--- app/jobs/tickets/generate_club_monthly_tickets_job.rb
+++ app/jobs/tickets/generate_club_monthly_tickets_job.rb
@@ -3,14 +3,14 @@ module Tickets
     queue_as :tickets
     sidekiq_options retry: 3
 
-    def perform(user, platform, quantity, integration)
-      return unless user.club_member?
+    def perform(user:, platform:, quantity:, integration:)
+      return unless Users::VerifyClubMembership.call(user:).result
 
       GenerateClubTickets.call(user:, platform:, quantity:, category: :monthly, integration:)
 
-      Tickets::SendMonthlyTicketsNotificationJob
+      Tickets::GenerateClubMonthlyTicketsJob
         .set(wait_until: 1.month.from_now)
-        .perform_later(user, platform, quantity, integration)
+        .perform_later(user:, platform:, quantity:, integration:)
     end
   end
 end
