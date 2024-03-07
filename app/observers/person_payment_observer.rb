e118160f6962acf03133f2c7040d5193fea506c9
mendesmcmg <claraplanejamento@gmail.com>
2024-03-07 17:39:49 -0300

    feat: calls monthly tickets job on payment flow

--- app/observers/person_payment_observer.rb
+++ app/observers/person_payment_observer.rb
@@ -1,10 +1,8 @@
 class PersonPaymentObserver < ActiveRecord::Observer
   def after_update(person_payment)
-    Mailers::SendPersonPaymentEmailJob.perform_later(person_payment:) if processing_to_paid?(person_payment)
-
-    if processing_to_failed?(person_payment)
-      Events::PersonPayments::SendFailedPaymentEventJob.perform_later(person_payment:)
-    end
+    send_person_payment_email(person_payment) if processing_to_paid?(person_payment)
+    give_monthly_tickets(person_payment) if processing_to_paid?(person_payment)
+    send_failed_payment_event(person_payment) if processing_to_failed?(person_payment)
   rescue StandardError
     nil
   end
@@ -20,5 +18,27 @@ class PersonPaymentObserver < ActiveRecord::Observer
     person_payment.previous_changes[:status] == %w[processing failed] &&
       person_payment.failed? &&
       person_payment.subscription.present?
+  end
+
+  private
+
+  def send_person_payment_email(person_payment)
+    Mailers::SendPersonPaymentEmailJob.perform_later(person_payment:)
+  end
+
+  def send_failed_payment_event(person_payment)
+    Events::PersonPayments::SendFailedPaymentEventJob.perform_later(person_payment:)
+  end
+
+  def give_monthly_tickets(person_payment)
+    user = person_payment.payer.user
+    return unless User::VerifyClubMembership(user).result
+
+    Tickets::GenerateClubMonthlyTicketsJob.perform_later(
+      user:,
+      platform: person_payment.subscription.platform,
+      quantity: person_payment.subscription.offer.plan.monthly_tickets,
+      integration: person_payment.integration
+    )
   end
 end
