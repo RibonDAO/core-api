1939702890859a35c6ac2f071db91c8dbfa56df3
letbueno <leticia.bueno.martins@gmail.com>
2024-03-13 10:17:17 -0300

    fix: move to open route

--- app/controllers/api/v1/tickets/tickets_controller.rb
+++ app/controllers/api/v1/tickets/tickets_controller.rb
@@ -8,6 +8,17 @@ module Api
           render json: { tickets: cached_tickets }, status: :ok
         end
 
+        def to_collect
+          return unless user
+         
+          @tickets = user.tickets.where(status: :to_collect, source: tickets_params[:source])
+          @daily_tickets = @tickets.where(category: :daily).count
+
+          @monthly_tickets = @tickets.where(category: :monthly).count
+
+          render json: { daily_tickets: @daily_tickets, monthly_tickets: @monthly_tickets }, status: :ok
+        end
+
         private
 
         def user
@@ -25,6 +36,10 @@ module Api
 
         def database_tickets
           Ticket.where(user:, status: :collected).count
+        end
+
+        def tickets_params
+          params.permit(:source)
         end
       end
     end
--- spec/requests/api/v1/tickets/tickets_controller_spec.rb
+++ spec/requests/api/v1/tickets/tickets_controller_spec.rb
@@ -44,4 +44,23 @@ RSpec.describe 'Api::V1::Tickets::Tickets', type: :request do
       end
     end
   end
+
+  describe 'get /tickets/to_collect' do
+    include_context 'when making a user request' do
+      subject(:request) { get '/api/v1/tickets/to_collect', headers: { Email: user.email } , params: { source: 'club' } }
+    end
+
+    let(:user) { create(:user) }
+
+    before do
+      create_list(:ticket, 10, user:, status: :to_collect, category: :daily, source: :club)
+      create_list(:ticket, 5, user:, status: :to_collect, category: :monthly, source: :club)
+    end
+
+    it 'returns the quantity of tickets to_collect for that user' do
+      request
+
+      expect(response.body).to eq({ daily_tickets: 10, monthly_tickets: 5 }.to_json)
+    end
+  end
 end
