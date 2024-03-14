ad33617cda4db7f852e9d17e4bf2f71d4ad6b869
Juju <juju@ribon.io>
2024-03-14 11:35:22 -0300

    fix: check if subscription its from club

--- app/observers/person_payment_observer.rb
+++ app/observers/person_payment_observer.rb
@@ -2,7 +2,7 @@ class PersonPaymentObserver < ActiveRecord::Observer
   def after_update(person_payment)
     Mailers::SendPersonPaymentEmailJob.perform_later(person_payment:) if processing_to_paid?(person_payment)
 
-    if person_payment.previous_changes[:status] == %w[processing paid] && person_payment.subscription
+    if processing_to_paid_subscription?(person_payment)
       Events::PersonPayments::SendSuccededPaymentEventJob.perform_later(person_payment:)
     end
 
@@ -15,13 +15,19 @@ class PersonPaymentObserver < ActiveRecord::Observer
 
   def after_create(person_payment)
     if person_payment.paid? && person_payment.subscription?
-      Events::Club::SendSuccededPaymentEventJob.perform_later(person_payment:)
+      Events::Club::SendSuccededPaymen tEventJob.perform_later(person_payment:)
     end
     if person_payment.failed? && person_payment.subscription?
       Events::Club::SendFailedPaymentEventJob.perform_later(person_payment:)
     end
   rescue StandardError
     nil
+  end
+
+  def processing_to_paid__club_subscription?(person_payment)
+    person_payment.previous_changes[:status] == %w[processing paid] &&
+      person_payment.paid? &&
+      person_payment.subscription.category == 'club'
   end
 
   def processing_to_paid?(person_payment)
