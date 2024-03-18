4f7d2ba456effc71a72e653417e86ecd222c38b1
Thais Correa <thais@ribon.io>
2024-03-18 17:48:04 -0300

    fix: test

--- spec/observers/ticket_observer_spec.rb
+++ spec/observers/ticket_observer_spec.rb
@@ -1,2 +1,41 @@
 # not being used
 require 'rails_helper'
+
+# rubocop:disable RSpec/EmptyExampleGroup
+RSpec.describe TicketObserver, type: :observer do
+  # describe 'if a ticket is created' do
+  #   let(:user) { create(:user) }
+  #   let(:ticket) { build(:ticket, user:) }
+  #   let(:key) { "tickets-#{user.id}" }
+
+  #   before do
+  #     allow(RedisStore::HStore).to receive(:get).with(key:).and_return(1)
+  #     allow(RedisStore::HStore).to receive(:set)
+  #   end
+
+  #   it 'updates cached tickets' do
+  #     ticket.save
+  #     expect(RedisStore::HStore).to have_received(:get).with(key:)
+  #     expect(RedisStore::HStore).to have_received(:set).with(key:, value: 2)
+  #   end
+  # end
+
+  # describe 'if a ticket is destroyed' do
+  #   let(:user) { create(:user) }
+  #   let(:ticket) { build(:ticket, user:) }
+  #   let(:key) { "tickets-#{user.id}" }
+
+  #   before do
+  #     ticket.save
+  #     allow(RedisStore::HStore).to receive(:get).with(key:).and_return(10)
+  #     allow(RedisStore::HStore).to receive(:set)
+  #   end
+
+  #   it 'updates cached tickets' do
+  #     ticket.destroy
+  #     expect(RedisStore::HStore).to have_received(:get).with(key:)
+  #     expect(RedisStore::HStore).to have_received(:set).with(key:, value: 9)
+  #   end
+  # end
+end
+# rubocop:enable RSpec/EmptyExampleGroup
