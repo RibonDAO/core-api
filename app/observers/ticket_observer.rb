50d6861c5e8bcfa5d2b0464df1a69ae749478dfa
Thais Correa <thais@ribon.io>
2024-03-18 17:34:26 -0300

    fix: test

--- app/observers/ticket_observer.rb
+++ app/observers/ticket_observer.rb
@@ -1,33 +1,33 @@
 # not being used
-class TicketObserver < ActiveRecord::Observer
-  def after_create(ticket)
-    cached_tickets = RedisStore::HStore.get(key: "tickets-#{ticket.user_id}") || database_tickets(ticket.user)
-    if ticket.status == 'collected'
-      RedisStore::HStore.set(key: "tickets-#{ticket.user_id}",
-                             value: cached_tickets + 1)
-    end
-  rescue StandardError
-    nil
-  end
+# class TicketObserver < ActiveRecord::Observer
+#   def after_create(ticket)
+#     cached_tickets = RedisStore::HStore.get(key: "tickets-#{ticket.user_id}") || database_tickets(ticket.user)
+#     if ticket.status == 'collected'
+#       RedisStore::HStore.set(key: "tickets-#{ticket.user_id}",
+#                              value: cached_tickets + 1)
+#     end
+#   rescue StandardError
+#     nil
+#   end
 
-  def after_destroy(ticket)
-    cached_tickets = RedisStore::HStore.get(key: "tickets-#{ticket.user_id}") || database_tickets(ticket.user)
-    RedisStore::HStore.set(key: "tickets-#{ticket.user_id}", value: cached_tickets - 1)
-  rescue StandardError
-    nil
-  end
+#   def after_destroy(ticket)
+#     cached_tickets = RedisStore::HStore.get(key: "tickets-#{ticket.user_id}") || database_tickets(ticket.user)
+#     RedisStore::HStore.set(key: "tickets-#{ticket.user_id}", value: cached_tickets - 1)
+#   rescue StandardError
+#     nil
+#   end
 
-  def after_update(ticket)
-    cached_tickets = RedisStore::HStore.get(key: "tickets-#{ticket.user_id}") || 0
-    if ticket.status == 'collected'
-      RedisStore::HStore.set(key: "tickets-#{ticket.user_id}",
-                             value: cached_tickets + 1)
-    end
-  rescue StandardError
-    nil
-  end
+#   def after_update(ticket)
+#     cached_tickets = RedisStore::HStore.get(key: "tickets-#{ticket.user_id}") || 0
+#     if ticket.status == 'collected'
+#       RedisStore::HStore.set(key: "tickets-#{ticket.user_id}",
+#                              value: cached_tickets + 1)
+#     end
+#   rescue StandardError
+#     nil
+#   end
 
-  def database_tickets(user)
-    Ticket.where(user:, status: :collected).count
-  end
-end
+#   def database_tickets(user)
+#     Ticket.where(user:, status: :collected).count
+#   end
+# end
