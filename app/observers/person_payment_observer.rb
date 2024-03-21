89d593156033cf0e0671bda330c62f9aaba1843b
Thais Correa <thais@ribon.io>
2024-03-21 11:25:42 -0300

    fix: conflicts

--- app/observers/person_payment_observer.rb
+++ app/observers/person_payment_observer.rb
@@ -2,10 +2,10 @@ class PersonPaymentObserver < ActiveRecord::Observer
   def after_update(person_payment)
     send_person_payment_email(person_payment) if paid_non_subscription?(person_payment)
     send_failed_payment_event(person_payment) if processing_to_failed?(person_payment)
-
     if paid_club_subscription?(person_payment)
       send_succeded_club_payment_event(person_payment)
       give_monthly_tickets(person_payment)
+      give_daily_tickets(person_payment)
     end
   rescue StandardError
     nil
