c315b6c768e0f3b52f8bc586705b90a6d9a10bea
mendesmcmg <claraplanejamento@gmail.com>
2024-03-21 13:49:07 -0300

    fix: test

--- spec/commands/tickets/clear_old_integration_tickets_spec.rb
+++ spec/commands/tickets/clear_old_integration_tickets_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe Tickets::ClearOldIntegrationTickets do
   subject(:command) { described_class.call(time:) }
 
   let(:time) { 1.month.ago }
+  let(:integration) { create(:integration) }
 
   before do
-    create(:ticket, created_at: 2.months.ago)
-    create(:ticket, created_at: 1.week.ago)
+    create(:user_integration_collected_ticket, integration:, created_at: 2.months.ago)
+    create(:user_integration_collected_ticket, integration:, created_at: 1.week.ago)
   end
 
   it 'deletes old tickets' do
-    expect { command }.to change(Ticket, :count).from(2).to(1)
+    expect { command }.to change(UserIntegrationCollectedTicket, :count).from(2).to(1)
   end
 end
