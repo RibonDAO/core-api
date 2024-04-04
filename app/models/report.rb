5bfcd747d8994e69f41803700c069159b39cdff2
Jean Scalabrin <jean.scalabrin@koud.com.br>
2024-04-04 17:15:45 -0300

    feat: add translate to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,5 +10,9 @@
 #  updated_at :datetime         not null
 #
 class Report < ApplicationRecord
+  extend Mobility
+
+  translates :name, type: :string
+
   validates :name, :link, presence: true
 end
