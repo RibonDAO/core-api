98f17f51ea1256c6919bd1bee5488599bf088f48
letbueno <leticia.bueno.martins@gmail.com>
2024-04-11 16:25:33 -0300

    feat: add coupons

---
+++ app/models/coupon.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: coupons
+#
+#  id                 :uuid             not null, primary key
+#  available_quantity :integer
+#  expiration_date    :datetime
+#  number_of_tickets  :integer
+#  reward_text        :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+class Coupon < ApplicationRecord
+  extend Mobility
+
+  translates :reward_text,  type: :string
+
+  validates :number_of_tickets, :reward_text, :expiration_date, presence: true
+end
