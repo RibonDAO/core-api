588806ead71507a8a7abe9c24645be551c8d4d6e
Thais Correa <thais@ribon.io>
2024-04-11 17:24:13 -0300

    feat: added WebhookJob event

--- app/commands/tickets/collect_by_external_ids.rb
+++ app/commands/tickets/collect_by_external_ids.rb
@@ -35,6 +35,7 @@ module Tickets
         create_voucher(external_id:)
         create_ticket(external_id:)
       end
+      call_webhook(voucher, ticket)
       ticket
     end
 
@@ -50,6 +51,10 @@ module Tickets
 
     def create_voucher(external_id:)
       @voucher = Voucher.create!(external_id:, integration:)
+    end
+
+    def call_webhook(voucher)
+      Vouchers::WebhookJob.perform_later(voucher, 'collected') if voucher.integration.webhook_url
     end
   end
 end
