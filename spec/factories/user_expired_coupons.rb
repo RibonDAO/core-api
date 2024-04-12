07599310b3ff114f9e72b76fe0b88692c56ca340
Thais Correa <thais@ribon.io>
2024-04-12 11:26:45 -0300

    feat: created userExpiredCoupon model

--- spec/factories/user_expired_coupons.rb
+++ spec/factories/user_expired_coupons.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: user_expired_coupons
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  coupon_id  :uuid             not null
+#  user_id    :bigint           not null
+#
 FactoryBot.define do
   factory :user_expired_coupon do
     user { build(:user) }
--- spec/models/user_expired_coupon_spec.rb
+++ spec/models/user_expired_coupon_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: user_expired_coupons
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  coupon_id  :uuid             not null
+#  user_id    :bigint           not null
+#
 require 'rails_helper'
 
 RSpec.describe UserExpiredCoupon, type: :model do
