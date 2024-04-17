aab8c28e7ffefee6882ceb05127cc6d1d35c42d7
Juju <juju@ribon.io>
2024-04-17 10:23:26 -0300

    fix: return first config

--- app/controllers/api/v1/configs/ribon_config_controller.rb
+++ app/controllers/api/v1/configs/ribon_config_controller.rb
@@ -3,7 +3,7 @@ module Api
     module Configs
       class RibonConfigController < ApplicationController
         def index
-          @ribon_config = RibonConfig.all
+          @ribon_config = RibonConfig.first
 
           render json: RibonConfigBlueprint.render(@ribon_config)
         end
