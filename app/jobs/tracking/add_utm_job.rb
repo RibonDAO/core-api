e16e2ae3d75dad9302ead1460efe5d48f7251467
Thais Correa <thais@ribon.io>
2024-04-18 16:24:27 -0300

    fix: retry

--- app/jobs/tracking/add_utm_job.rb
+++ app/jobs/tracking/add_utm_job.rb
@@ -1,6 +1,7 @@
 module Tracking
   class AddUtmJob < ApplicationJob
     queue_as :default
+    sidekiq_options retry: 3
 
     def perform(utm_params:, trackable:)
       AddUtm.call(utm_params:, trackable:)
