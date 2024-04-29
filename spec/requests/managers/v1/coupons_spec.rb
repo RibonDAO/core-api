3586a75c44e086b4b28266ec0fd24973cab11940
Nathiely Macedo <nathielymm@gmail.com>
2024-04-29 09:13:05 -0300

    test: adds coupon manager test

---
+++ spec/requests/managers/v1/coupons_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Managers::V1::Coupons', type: :request do
+  describe 'GET /index' do
+    subject(:request) { get '/managers/v1/coupons' }
+
+    before do
+      create_list(:coupon, 2)
+    end
+
+    it 'returns a list of coupons' do
+      request
+      expect_response_collection_to_have_keys(%w[id status available_quantity
+                                                 expiration_date number_of_tickets reward_text
+                                                 ticket_availability_in_minutes link])
+    end
+  end
+
+  describe 'GET /show' do
+    subject(:request) { get "/managers/v1/coupons/#{coupon.id}" }
+
+    let(:coupon) { create(:coupon) }
+
+    it 'returns a single coupon' do
+      request
+
+      expect_response_to_have_keys(%w[id status available_quantity
+                                      expiration_date number_of_tickets reward_text
+                                      ticket_availability_in_minutes link])
+    end
+  end
+end
