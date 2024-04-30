39723384ec9c819bc281bc3b152700ff119d329b
Nathiely Macedo <nathielymm@gmail.com>
2024-04-30 18:58:54 -0300

    test: add create coupon test

--- spec/requests/managers/v1/coupons_spec.rb
+++ spec/requests/managers/v1/coupons_spec.rb
@@ -29,4 +29,43 @@ RSpec.describe 'Managers::V1::Coupons', type: :request do
                                       link])
     end
   end
+
+  describe 'POST /create' do
+    subject(:request) { post '/managers/v1/coupons', params: }
+
+    context 'with the right params' do
+      let(:params) do
+        {
+          available_quantity: 10,
+          expiration_date: 1.day.from_now,
+          number_of_tickets: 1,
+          reward_text: 'New reward',
+          status: 'active'
+        }
+      end
+
+      it 'creates a new coupon' do
+        expect { request }.to change(Coupon, :count).by(1)
+      end
+
+      it 'returns a single coupon' do
+        request
+
+        expect_response_to_have_keys(%w[id status available_quantity
+                                        expiration_date number_of_tickets reward_text
+                                        link])
+      end
+    end
+
+    context 'with the wrong params' do
+      let(:params) do
+        { available_quantity: nil, expiration_date: nil, number_of_tickets: nil, reward_text: nil, status: nil }
+      end
+
+      it 'renders a error message' do
+        request
+        expect(response).to have_http_status :unprocessable_entity
+      end
+    end
+  end
 end
