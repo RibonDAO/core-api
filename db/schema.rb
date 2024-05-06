8a963bda312b341f13833e6fee8ffa99ee63e10d
Thais Correa <thais@ribon.io>
2024-05-06 10:15:35 -0300

    fix: schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_29_164824) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_151028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -220,7 +220,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_29_164824) do
     t.string "reward_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0
   end
 
   create_table "crypto_users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
