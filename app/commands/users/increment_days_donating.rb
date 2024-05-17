310296021cfb483d3206fe7284f287360e882eac
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-05-17 10:36:23 -0300

    feat: adds days donating command

---
+++ app/commands/users/increment_days_donating.rb
@@ -0,0 +1,28 @@
+module Users
+  class IncrementDaysDonating < ApplicationCommand
+    prepend SimpleCommand
+
+    attr_reader :user
+
+    def initialize(user:)
+      @user = user
+    end
+
+    def call
+      with_exception_handle do
+        return if donated_today?
+
+        @user.user_donation_stats.increment(:days_donating, 1)
+      end
+    end
+
+    private
+
+    def donated_today?
+      today_date = Time.zone.now.to_date
+      last_donation_date = @user.user_donation_stats.last_donation_at.to_date
+
+      today_date == last_donation_date
+    end
+  end
+end
