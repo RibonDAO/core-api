4dba7725cad8ff73e9d05e686fdcf401d517042d
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-05-20 16:08:20 -0300

    feat: adds redis cache in case of failure

--- app/jobs/users/update_days_donating_for_all_job.rb
+++ app/jobs/users/update_days_donating_for_all_job.rb
@@ -2,6 +2,8 @@ module Users
   class UpdateDaysDonatingForAllJob < ApplicationJob
     queue_as :default
 
+    REDIS_KEY = 'last_updated_user_id'.freeze
+
     def perform
       user_batches do |user_batch|
         user_donation_stats = user_batch.map(&:user_donation_stats)
@@ -9,19 +11,41 @@ module Users
         update_list = update_list(user_donation_stats)
 
         UserDonationStats.update(update_list.keys, update_list.values)
+
+        last_user = update_list.keys.last
+        update_checkpoint(last_user) if last_user.present?
       end
     end
 
     private
 
+    def update_checkpoint(last_user_id)
+      RedisStore::HStore.set(key: REDIS_KEY, value: last_user_id)
+    end
+
+    def last_user_id
+      RedisStore::HStore.get(key: REDIS_KEY)
+    end
+
     # Finds users in batches of 1000.
     # Finds only users with donations.
     # Avoids n + 1 queries by including user_donation_stats.
+    # If user id cache is present, start from it
     def user_batches(&)
-      User.joins(:donations)
+      if last_user_id.present?
+        User
+          .where('users.id > ?', last_user_id)
+          .joins(:donations)
           .group('users.id')
           .includes(:user_donation_stats)
           .find_in_batches(batch_size: 1000, &)
+      else
+        User
+          .joins(:donations)
+          .group('users.id')
+          .includes(:user_donation_stats)
+          .find_in_batches(batch_size: 1000, &)
+      end
     end
 
     def update_list(donation_stats)
--- spec/jobs/users/update_days_donating_for_all_job_spec.rb
+++ spec/jobs/users/update_days_donating_for_all_job_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Users::UpdateDaysDonatingForAllJob, type: :job do
   subject(:perform_job) { described_class.perform_now }
 
+  let(:redis_key) { 'last_updated_user_id' }
+
   describe 'when user has donated on multiple different days' do
     let(:user) { create(:user) }
 
@@ -10,6 +12,9 @@ RSpec.describe Users::UpdateDaysDonatingForAllJob, type: :job do
       create(:donation, created_at: Time.zone.now.advance(days: -1), user:)
       create(:donation, created_at: Time.zone.now.advance(days: -2), user:)
       create(:donation, created_at: Time.zone.now.advance(days: -3), user:)
+
+      allow(RedisStore::HStore).to receive(:get).and_return(nil)
+      allow(RedisStore::HStore).to receive(:set)
     end
 
     it 'updates days donating to 3' do
@@ -24,6 +29,9 @@ RSpec.describe Users::UpdateDaysDonatingForAllJob, type: :job do
       create(:donation, created_at: Time.zone.now.advance(days: -1), user:)
       create(:donation, created_at: Time.zone.now.advance(days: -2), user:)
       create(:donation, created_at: Time.zone.now.advance(days: -2), user:)
+
+      allow(RedisStore::HStore).to receive(:get).and_return(nil)
+      allow(RedisStore::HStore).to receive(:set)
     end
 
     it 'updates days donating to 2' do
@@ -34,8 +42,37 @@ RSpec.describe Users::UpdateDaysDonatingForAllJob, type: :job do
   describe 'when user has not donated' do
     let(:user) { create(:user) }
 
+    before do
+      allow(RedisStore::HStore).to receive(:get).and_return(nil)
+      allow(RedisStore::HStore).to receive(:set)
+    end
+
     it 'does not change days donating' do
       expect { perform_job }.not_to change { user.user_donation_stats.reload.days_donating }
+    end
+  end
+
+  describe 'when there is a last_user cache' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+
+    before do
+      create(:donation, created_at: Time.zone.now.advance(days: -1), user: user1)
+      create(:donation, created_at: Time.zone.now.advance(days: -2), user: user2)
+      create(:donation, created_at: Time.zone.now.advance(days: -2), user: user2)
+
+      allow(RedisStore::HStore).to receive(:get).and_return(user1.id)
+      allow(RedisStore::HStore).to receive(:set)
+    end
+
+    it 'doesnt update the cached user' do
+      expect { perform_job }
+        .not_to change { user1.user_donation_stats.reload.days_donating }
+    end
+
+    it 'updates the non cached user' do
+      expect { perform_job }
+        .to change { user2.user_donation_stats.reload.days_donating }
     end
   end
 end
