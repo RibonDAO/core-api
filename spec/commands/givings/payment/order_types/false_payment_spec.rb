2b2b68165c4d69d39c2a0279b2a7f7e83e0fa0b8
Jean Scalabrin <jean.scalabrin@koud.com.br>
2024-05-28 15:30:35 -0300

    subscription with false payment

---
+++ spec/commands/givings/payment/order_types/false_payment_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Givings::Payment::OrderTypes::FalsePayment do
+  describe '.call' do
+    subject(:command) { described_class.new(args) }
+
+    include_context('when mocking a request') { let(:cassette_name) { 'stripe_payment_method' } }
+
+    context 'when it is a club subscription' do
+      let(:integration) { create(:integration) }
+      let(:offer) { create(:offer) }
+      let(:args) do
+        { email: 'user@test.com', offer:, name: 'teste', integration_id: integration.id}
+      end
+
+      it 'creates a subscription with receiver nil' do
+        command.call
+        expect(Subscription.last.receiver).to be_nil
+      end
+    end
+  end
+end
