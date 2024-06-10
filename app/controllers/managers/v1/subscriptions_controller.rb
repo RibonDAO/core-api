8907714f91685a207135e20a66bae8a0436986ea
mendesmcmg <claraplanejamento@gmail.com>
2024-06-10 16:48:57 -0300

    feat: adds create subscriptions command

--- app/controllers/managers/v1/subscriptions_controller.rb
+++ app/controllers/managers/v1/subscriptions_controller.rb
@@ -1,17 +1,19 @@
 module Managers
   module V1
     class SubscriptionsController < ManagersController
-      require 'csv'
 
       def upload_csv_and_create_subscriptions
-        # byebug
         emails = params[:csv_content].split("\r\n")[1..]
         offer_id = params[:offer_id]
         integration_id = params[:integration_id]
-        if file
-          render json: { message: 'Subscriptions created successfully' }, status: :ok
+        offer = Offer.find(offer_id)
+
+        result = create_subscriptions(emails, offer, integration_id)
+
+        if result.success?
+          render json: { message: 'Subscriptions created successfully' }, status: :created
         else
-          render json: { error: 'No file uploaded' }, status: :unprocessable_entity
+          render json: { message: 'Error creating subscriptions' }, status: :unprocessable_entity
         end
       end
 
@@ -19,6 +21,18 @@ module Managers
 
       def subscription_params
         params.permit(:csv_content, :offer_id, :integration_id, :test)
+      end
+
+      def create_subscriptions(emails, offer, integration_id)
+        emails.each do |email|
+          args = {
+            email:,
+            offer:,
+            integration_id:
+          }
+
+          ::Givings::Subscriptions::CreateDirectTransferSubscription.new(args).call
+        end
       end
     end
   end
