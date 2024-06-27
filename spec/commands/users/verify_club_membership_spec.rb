cffaaad062029fd65c9df47d4fc6205924a56c89
letbueno <leticia.bueno.martins@gmail.com>
2024-06-27 13:38:20 -0300

    fix: is member and filter of subscriptions

--- spec/commands/users/verify_club_membership_spec.rb
+++ spec/commands/users/verify_club_membership_spec.rb
@@ -6,23 +6,19 @@ describe Users::VerifyClubMembership do
     subject(:command) { described_class.call(user:) }
 
     let(:user) { create(:user) }
+    let(:customer) { create(:customer, user:) }
+    let(:offer) { create(:offer, category: :club) }
+    let(:subscription) { create(:subscription, payer: customer, offer:, status: :active) }
+    let!(:person_payment) { create(:person_payment, payer: customer, subscription:) }
 
     context 'when user is a club member' do
-      before do
-        create(:customer, user:)
-        create(:subscription, payer: user.customers.first, status: :active, offer: create(:offer, category: :club))
-      end
-
       it 'returns true' do
         expect(command.result).to be_truthy
       end
     end
 
     context 'when user is not a club member' do
-      before do
-        create(:customer, user:)
-        create(:subscription, payer: user.customers.first, status: :active, offer: create(:offer))
-      end
+      let(:subscription) { create(:subscription, payer: customer, offer:, status: :inactive) }
 
       it 'returns false' do
         expect(command.result).to be_falsey
