7e8fab185f7e6c4bb5ff5d83fcf6cdd4e74f005e
letbueno <leticia.bueno.martins@gmail.com>
2024-06-27 13:52:59 -0300

    fix: linter

--- spec/commands/users/verify_club_membership_spec.rb
+++ spec/commands/users/verify_club_membership_spec.rb
@@ -9,9 +9,14 @@ describe Users::VerifyClubMembership do
     let(:customer) { create(:customer, user:) }
     let(:offer) { create(:offer, category: :club) }
     let(:subscription) { create(:subscription, payer: customer, offer:, status: :active) }
-    let!(:person_payment) { create(:person_payment, payer: customer, subscription:) }
+
 
     context 'when user is a club member' do
+
+      before do
+        create(:person_payment, payer: customer, subscription:)
+      end
+
       it 'returns true' do
         expect(command.result).to be_truthy
       end
@@ -20,6 +25,9 @@ describe Users::VerifyClubMembership do
     context 'when user is not a club member' do
       let(:subscription) { create(:subscription, payer: customer, offer:, status: :inactive) }
 
+      before do
+        create(:person_payment, payer: customer, subscription:)
+      end
       it 'returns false' do
         expect(command.result).to be_falsey
       end
