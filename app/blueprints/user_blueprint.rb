2cb27fc22d6510ae1d753bc87919bbd14593d0a0
mendesmcmg <claraplanejamento@gmail.com>
2024-07-04 18:23:45 -0300

    feat: adds company to user model

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -5,5 +5,9 @@ class UserBlueprint < Blueprinter::Base
 
   view :extended do
     fields :last_donation_at, :last_donated_cause
+
+    field(:company) do |object|
+      IntegrationBlueprint.render_as_hash(object.company) if object.company
+    end
   end
 end
