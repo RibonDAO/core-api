f837fef642752d20b8b600bb5c56136ceedbe81f
Henrique Barbosa <henrikleod@gmail.com>
2024-07-16 16:43:04 -0300

    feat: otp code service

---
+++ app/services/auth/otp_code_service.rb
@@ -0,0 +1,50 @@
+module Auth
+  class OtpCodeService
+    attr_reader :authenticatable
+
+    SCOPE_MAPPING = {
+      BigDonor => %i[patrons app],
+      Account => %i[deeplink auth]
+    }.freeze
+
+    def initialize(authenticatable:)
+      @authenticatable = authenticatable
+      @scope = SCOPE_MAPPING[authenticatable.class] || 'unscoped'
+    end
+
+    def find_or_create_otp_code
+      current_otp_code || create_otp_code
+    end
+
+    def valid_otp_code?(code)
+      return false if current_otp_code != code
+
+      destroy_otp_code
+      true
+    end
+
+    private
+
+    def current_otp_code
+      RedisStore::HStore.get(key: "auth_otp_code_#{authenticatable.class.name}_#{authenticatable.id}")
+    end
+
+    def create_otp_code
+      RedisStore::HStore.set(key: "auth_otp_code_#{authenticatable.class.name}_#{authenticatable.id}",
+                             value: SecureRandom.hex[0,5], expires_in: token_expiration(@scope))
+    end
+
+    def destroy_otp_code
+      RedisStore::HStore.del(key: "auth_otp_code_#{authenticatable.class.name}_#{authenticatable.id}")
+    end
+
+    def token_expiration(scope)
+      case scope
+      when %i[patrons app]
+        1.month
+      else
+        30.minutes
+      end
+    end
+  end
+end
