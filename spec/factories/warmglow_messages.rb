139bea5a25b3a7b4f03742a70bdd93c764b1c9c3
Henrique Barbosa <henrikleod@gmail.com>
2024-07-17 12:21:40 -0300

    chore: add missing annotations

--- spec/factories/warmglow_messages.rb
+++ spec/factories/warmglow_messages.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: warmglow_messages
+#
+#  id         :bigint           not null, primary key
+#  message    :text
+#  status     :integer          default("inactive")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :warmglow_message do
     message { 'Message 1' }
