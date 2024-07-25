95bf4f2a67c37766aa9f3ffe7541b2b49e6fb03d
Henrique Barbosa <henrikleod@gmail.com>
2024-07-25 17:18:33 -0300

    feat: cdn

--- spec/helpers/images_helper_spec.rb
+++ spec/helpers/images_helper_spec.rb
@@ -10,27 +10,46 @@ RSpec.describe ImagesHelper do
 
     let(:image) { build(:story, :with_image).image }
 
-    context 'when a variant is passed' do
-      let(:variant) do
-        { resize_to_fit: [500, 500], format: :jpg }
+    context 'when CDN is enabled' do
+      before do
+        allow(ENV).to receive(:fetch).with('CDN_ENABLED', 'false').and_return('true')
+        allow(ENV).to receive(:fetch).with('CDN_URL', nil).and_return('https://cdn.com/')
       end
 
-      it 'calls the Rails polymorphic url with correct params' do
-        allow(image).to receive(:variant).and_return(image)
-        described_class.image_url_for(image, variant:)
+      let(:subject) { described_class.image_url_for(image) }
 
-        expect(image).to have_received(:variant).with(variant)
-        expect(Rails.application.routes.url_helpers)
-          .to have_received(:polymorphic_url).with(image)
+      it 'calls the url_with_cdn method' do
+        expect(subject).to eq("https://cdn.com/#{image.key}")
       end
     end
 
-    context 'when no variant is passed' do
-      it 'calls the Rails polymorphic url with correct params' do
-        described_class.image_url_for(image)
+    context 'when CDN is disabled' do
+      before do
+        allow(ENV).to receive(:fetch).with('CDN_ENABLED', 'false').and_return('false')
+      end
 
-        expect(Rails.application.routes.url_helpers)
-          .to have_received(:polymorphic_url).with(image)
+      context 'when a variant is passed' do
+        let(:variant) do
+          { resize_to_fit: [500, 500], format: :jpg }
+        end
+  
+        it 'calls the Rails polymorphic url with correct params' do
+          allow(image).to receive(:variant).and_return(image)
+          described_class.image_url_for(image, variant:)
+  
+          expect(image).to have_received(:variant).with(variant)
+          expect(Rails.application.routes.url_helpers)
+            .to have_received(:polymorphic_url).with(image)
+        end
+      end
+  
+      context 'when no variant is passed' do
+        it 'calls the Rails polymorphic url with correct params' do
+          described_class.image_url_for(image)
+  
+          expect(Rails.application.routes.url_helpers)
+            .to have_received(:polymorphic_url).with(image)
+        end
       end
     end
   end
